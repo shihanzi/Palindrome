45f0f2a7b8b730068f5ef103c70a8d31178d80cd
shihanze <shihanmails@gmail.com>
2024-08-29 08:38:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("please type");
+string input = Console.ReadLine();
+var reversedString = Reverse(input);
+
+if (input == reversedString)
+{
+    Console.WriteLine("It is a plaindrome");
+
+}
+else
+{
+    Console.WriteLine("It is not a plaindrome");
+
+}
+
+static string Reverse(string s)
+{
+    char[] charArray = s.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
+
